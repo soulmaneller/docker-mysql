f6b1691a7188eae54a417edf850bce446830cfb4
Sitthidet <sitthidet.chaksu@gmail.com>
2021-04-08 20:09:28 +0700

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "Copy config files"
 cp -a /root/config/* /etc/mysql/conf.d/
+
+echo "Chmod config files"
 chmod 0444 /etc/mysql/conf.d/*.cnf
+
+echo "Create a login path for root. password: ${MYSQL_ROOT_PASSWORD}"
 echo $MYSQL_ROOT_PASSWORD | mysql_config_editor set --login-path=root --host=localhost --user=root --password
 
 /entrypoint.sh mysqld
